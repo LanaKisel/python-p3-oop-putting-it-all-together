7149b6e2605b2c552ea9b537b647f5953cbc9bf9
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-01-19 16:42:21 -0800

    decorator

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title="pride and prejudice", page_count=496):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(page_count):
+        print("Flipping the page...wow, you read fast!")
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")   
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @condition.setter
+    def condition(self, condition):
+       self._condition = condition   
+
+    def cobble(self):
+        print("Your shoe is as good as new!")  
+        self.condition = "New"
